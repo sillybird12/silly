ef3780a808d60ac63453253a4501592900b8b30b
sillybird12 <111685971+sillybird12@users.noreply.github.com>
2022-08-22 10:29:19 +0800

    Update main.py

--- main.py
+++ main.py
@@ -15,8 +15,6 @@ app_id = os.environ["APP_ID"]
 app_secret = os.environ["APP_SECRET"]
 
 user_id = os.environ["USER_ID"]
-user_id = os.environ["USER_ID"]
-user_id = os.environ["USER_ID"]
 template_id = os.environ["TEMPLATE_ID"]
 
 
